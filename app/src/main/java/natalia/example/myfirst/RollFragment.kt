033b3712de0641b87fdb46709a57a34e7cb749fb
Natalia <natal.kozyreva@gmail.com>
2021-12-10 04:05:03 +0300

    magic time

--- app/src/main/java/natalia/example/myfirst/RollFragment.kt
+++ app/src/main/java/natalia/example/myfirst/RollFragment.kt
@@ -9,13 +9,14 @@ import androidx.fragment.app.Fragment
 import android.widget.TextView
 import kotlin.random.Random
 
+private const val LAST_ROLLED_MAGIC = "magic"
 
 class RollFragment : Fragment() {
 
 
     lateinit var textV: TextView
     lateinit var rollButton: Button
-
+    var magicTexts: List<String> = listOf("")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,14 +26,39 @@ class RollFragment : Fragment() {
         textV = view.findViewById(R.id.textViewRoll)
         rollButton = view.findViewById(R.id.rollButton)
 
+        if (savedInstanceState != null)
+            textV.text = savedInstanceState.getString(LAST_ROLLED_MAGIC)
+
+
+        magicTexts = listOf(
+            "Вам пора отдохнуть",
+            "Вас ждет приятный сюрприз",
+            "Если Вы проявите инициативу, успех не заставит себя ждать",
+            "Вас ждет приятный сюрприз",
+            "Сегодня как раз наступило то завтра, о котором Вы беспокоились вчера",
+            "Ждите перемен",
+            "Вас ждет встреча с важным человеком",
+            "Проблема не там, где Вам кажется",
+            "Не поддавайтесь эмоциям",
+            "Будьте настойчивы в битве с собственным эгоизмом",
+            "Подождите немножко, и следующий день принесет вам неожиданную долгожданную радость",
+            "Пришло время действовать",
+            "События полностью вне Вашего контроля"
+        )
+
+
+
         rollButton.setOnClickListener {
-            val randomValues = List(10) { Random.nextInt(0, 100) }
-            textV.text = randomValues.random().toString()
+
+            textV.text = magicTexts.random()
         }
 
         return view
     }
 
-
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(LAST_ROLLED_MAGIC, textV.text.toString())
+    }
 
 }
